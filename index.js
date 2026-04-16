70d278e0672cbc06ffc05e8b66e631961e96fcdc
somegrace-byte <somegrace@gmail.com>
2026-04-16 21:07:50 +1200

    Changed max registration attempts

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ const registerLimiter = rateLimit({
 //Registration sucess limiter 
 const registerSuccessLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour
-  max: 3,
+  max: 10,
   message: { error: "Too many registration attempts. Try again later." }
 });
 
